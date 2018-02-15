9ccf5d145307249cddc33d63974b0fe3c49394ee
cgroov <30381292+groov1234@users.noreply.github.com>
2018-02-15 15:27:57 -0500

    blankly staring at Ivan

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-const port = require("./config.js");
-const apiKey = require('./config.js');
-const gmaps = require("./config.js");
+
+const config = require('./config.js');
+const port = config.port;
+const key = config.gmaps.apiKey;
 
 const server = express();
 
 server.use(bodyParser.json());
 
-fetch(apiKey)
-  .then(res => res.json())
-  .then(json => console.log(json));
+const STATUS_SUCCESS = 200;
+const STATUS_USER_ERROR = 422;
+
+server.use(bodyParser.json());
+
+server.post('/place', (req, res) => {
+  if (!req.body.search) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'No search query provided' });
+    return;
+  }
+
+  const URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=Pizza+in+Newton&key=${key}`;
+
+  let data = fetch(URL)
+    .then(res => res.json())
+    .then(json => json)
+    .catch(err => console.log(err));
+    res.status(STATUS_SUCCESS);
+    res.json(data);
+  });
 
 server.listen(port, err => {
   if (err) {
@@ -20,3 +39,4 @@ server.listen(port, err => {
     console.log(`Server Listening on Port ${port}`);
   }
 });
+
